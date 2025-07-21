1dacc34bc5f61a991fec2befedc7b71704d41ffd
your_github_username <pokhrelameet12@gmail.com>
2025-07-21 16:17:59 +0545

    add new dart page for home

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -3,11 +3,13 @@ import 'package:modernloginpage/screens/auth/login_screen.dart';
 import 'package:modernloginpage/screens/auth/signup_screen.dart';
 import 'package:modernloginpage/screens/initial_splash_screen.dart';
 import 'package:modernloginpage/screens/splash_screen.dart';
+import  'package:modernloginpage/screens/home_screen.dart';
+
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
     '/initial': (context) => const InitialSplashScreen(),
-    '/': (context) => const SplashScreen(),
+    '/home': (context) => const HomeScreen(),  //
     '/login': (context) => const LoginScreen(),
     '/signup': (context) => const SignUpScreen(),
   };
